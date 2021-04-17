bef9952d29e71b8f64767019776d3e4b38bca07e
Alexandre Lopes <alexandre9999lopes@gmail.com>
2021-04-17 18:21:08 -0300

    updates lint config

--- dashboard/.eslintrc.js
+++ dashboard/.eslintrc.js
@@ -3,17 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'eol-last': ['error', 'never']
+    'eol-last': ['error', 'never'],
+    'vue/no-multiple-template-root': 'off'
   },
   overrides: [
     {
