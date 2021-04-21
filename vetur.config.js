d752ae61be19cd3d635c77446602eec38f679d18
Alexandre Lopes <alexandre9999lopes@gmail.com>
2021-04-21 11:12:10 -0300

    adds vetur config file

---
+++ vetur.config.js
@@ -0,0 +1,10 @@
+// vetur.config.js
+module.exports = {
+  settings: {
+    "vetur.useWorkspaceDependencies": true,
+    "vetur.experimental.templateInterpolationService": true
+  },
+  projects: [
+    './dashboard'
+  ]
+}
