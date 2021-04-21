e65b2e37aa73a70d8e7df9c193d3221895a38b38
Alexandre Lopes <alexandre9999lopes@gmail.com>
2021-04-21 14:51:36 -0300

    creates header logged component

---
+++ dashboard/src/components/HeaderLogged/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="flex items-center justify-between w-4/5 max-w-6-xl py-10">
+    <div class="w-28 lg:w-36">
+      <img class="w-full" src="../../assets/images/logo_white.png" alt="logo" />
+    </div>
+
+    <div class="flex">
+      <ul class="flex list-none">
+        <li
+          @click="() => router.push({ name: 'Credentials' })"
+          class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none"
+        >
+          Credentials
+        </li>
+        <li
+          @click="() => router.push({ name: 'Feedbacks' })"
+          class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none"
+        >
+          Feedbacks
+        </li>
+
+        <li
+          @click="handleLogout"
+          class="px-6 py-2 font-bold bg-white text-brand-main rounded-full cursor-pointer focus:outline-none"
+        >
+          {{ logoutLabel }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+import { useRouter } from 'vue-router'
+import useStore from '../../hooks/userStore'
+import { cleanCurrentUser } from '../../store/user'
+
+export default {
+  setup () {
+    const router = useRouter()
+    const store = useStore('User')
+
+    const logoutLabel = computed(() => {
+      if (!store.currentUser.name) {
+        return '...'
+      }
+
+      return `${store.currentUser.name} (sair)`
+    })
+
+    function handleLogout () {
+      window.localStorage.removeItem('token')
+      router.push({ name: 'Home' })
+      cleanCurrentUser()
+    }
+    return {
+      router,
+      logoutLabel,
+      handleLogout
+    }
+  }
+}
+</script>
