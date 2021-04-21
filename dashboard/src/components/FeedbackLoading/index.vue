240953c23ceb83108876f5efcceeffab83cd0e6a
Alexandre Lopes <alexandre9999lopes@gmail.com>
2021-04-21 18:01:50 -0300

    creates loading feedback component

---
+++ dashboard/src/components/FeedbackLoading/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <content-loader class="flex flex-col items-center rounded"
+    width="100%"
+    height="300px"
+  >
+    <content-loader
+      class="mt-3 rounded"
+      width="90%"
+      height="90px"
+      animation-duration="2s"
+    />
+
+    <content-loader
+      class="mt-3 rounded"
+      width="90%"
+      height="90px"
+      animation-duration="2.2s"
+    />
+
+    <content-loader
+      class="mt-3 rounded"
+      width="90%"
+      height="90px"
+      animation-duration="2.5s"
+    />
+  </content-loader>
+</template>
+
+<script>
+import ContentLoader from '../ContentLoader'
+
+export default {
+  components: {
+    ContentLoader
+  }
+}
+</script>
+
+<style>
+
+</style>
