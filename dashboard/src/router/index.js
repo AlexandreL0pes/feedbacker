2d5365ed63710229a127408df76b85d88d669b73
Alexandre Lopes <alexandre9999lopes@gmail.com>
2021-04-23 09:00:11 -0300

    export routes

--- dashboard/src/router/index.js
+++ dashboard/src/router/index.js
@@ -4,7 +4,7 @@ const Home = () => import('../views/Home/index.vue')
 const Feedbacks = () => import('../views/Feedbacks/index.vue')
 const Credentials = () => import('../views/Credentials/index.vue')
 
-const routes = [
+export const routes = [
   {
     path: '/',
     name: 'Home',
