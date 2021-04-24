e51dffe6e2abc238ccd706a4933289c3642e4677
Alexandre Lopes <alexandre9999lopes@gmail.com>
2021-04-24 16:06:59 -0300

    updates mock data

--- backend/database/mock.js
+++ backend/database/mock.js
@@ -2,8 +2,8 @@ module.exports = {
   users: [
     {
       id: 'eab759f8-f238-4ff9-ae91-ee1558982329',
-      name: 'Igor Halfeld',
-      email: 'igor@igor.me',
+      name: 'Alexandre Lopes',
+      email: 'flopes.alexandre@gmail.com',
       password: '1234',
       apiKey: ['fcd5015c-10d3-4e9c-b395-ec7ed8850165'],
       createdAt: new Date('2020-09-05').getTime()
